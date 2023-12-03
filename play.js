d79fefcb5719259aeb39a5703c9fb88a8156be92
윤승재 <sjyun011278@gmail.com>
2023-12-03 22:35:00 +0900

    변수 및 화살표 함수

---
+++ play.js
@@ -0,0 +1,6 @@
+const myName= 'seungjae'
+let age = 20
+const hasHobbies = true
+
+
+const summarizeUser = (userName, userAge, userHasHobbies) => 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobbies
