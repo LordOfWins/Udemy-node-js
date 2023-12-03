82ad0bbdd2b2da347be02141460057a25c328578
윤승재 <sjyun011278@gmail.com>
2023-12-04 01:13:00 +0900

    rest 연산자

--- play.js
+++ play.js
@@ -27,3 +27,15 @@ console.log(hobbies)
 
 hobbies.push('Programming')
 console.log(hobbies)
+
+const copiedArray = hobbies.slice()
+console.log(copiedArray)
+
+const copiedArray2 = [...hobbies]
+console.log(copiedArray2)
+
+const toArray = (...args) => {
+    return args
+}
+
+console.log(toArray(1, 2, 3, 4))
